e2c7dc5130f5350b19c545994ad3d18a0fe8771d
Andrew Drost <55260245+MasterZuse@users.noreply.github.com>
2026-04-01 21:16:04 -0500

    waveform struct

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
 #include "miniaudio.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct {
+    int8_t samples[16];
+} waveform_16;
 
 int count;
-float current;
+int max_count;
+waveform_16* wf;
 
-void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+void dataCallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
     float* outBuffer = (float*) pOutput;
     for (int i = 0; i < frameCount; i++) {
         int bufferIdx = i * 2;
-        outBuffer[bufferIdx] = current;
-        outBuffer[bufferIdx + 1] = current;
+        float val = (wf->samples[count / 10] * 0.1f) / 127.0f;
+        outBuffer[bufferIdx] = val;
+        outBuffer[bufferIdx + 1] = val;
         count++;
-        if (count > 120) {
+        if (count > max_count) {
             count = 0;
-            current *= -1.0f;
         }
     }
+}
+
+waveform_16* createWaveform() {
+    waveform_16* w = malloc(sizeof(waveform_16));
+    for (int i = 0; i < 16; i++) {
+        w->samples[i] = 127 - (16 * i);
+    }
+    return w;
 }
 
 int main() {
@@ -23,9 +38,10 @@ int main() {
     config.playback.format = ma_format_f32;
     config.playback.channels = 2;
     config.sampleRate = 48000;
-    config.dataCallback = data_callback;
+    config.dataCallback = dataCallback;
     count = 0;
-    current = 0.15f;
+    max_count = 160;
+    wf = createWaveform();
 
     ma_device device;
     if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) {
